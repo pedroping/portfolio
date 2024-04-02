92e0b2a620b1ed43e32a5849fb90d85d2e59c5a8
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-04-02 17:13:10 -0300

    Chore: all project external dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideAnimationsAsync(),
+  ],
 };
