27cdb2dc320716b94490df6ab785ae11fa34936b
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-05-09 08:38:52 -0300

    Feat: enter & leave animations

--- libs/utils/util-animations/src/lib/animations/animations.ts
+++ libs/utils/util-animations/src/lib/animations/animations.ts
@@ -1,17 +1,29 @@
 import { animate, style } from '@angular/animations';
 import { IAnimation, IAvailableAnimations } from '../models/animation-models';
 
-export const ENTER_ANIMATIONS_Y: IAnimation = [
+export const ENTER_ANIMATION_Y: IAnimation = [
   style({ opacity: 0, transform: 'translateY(100%)' }),
   animate(100, style({ opacity: 1, transform: 'translateY(0)' })),
 ];
 
-export const LEAVE_ANIMATIONS_Y: IAnimation = [
+export const LEAVE_ANIMATION_Y: IAnimation = [
   style({ opacity: 1, transform: 'translateY(0)' }),
   animate(100, style({ opacity: 1, transform: 'translateY(100%)' })),
 ];
 
+export const ENTER_ABOVE_ANIMATION_Y: IAnimation = [
+  style({ opacity: 0, transform: 'translateY(-100%)' }),
+  animate(100, style({ opacity: 1, transform: 'translateY(0)' })),
+];
+
+export const LEAVE_ABOVE_ANIMATION_Y: IAnimation = [
+  style({ opacity: 1, transform: 'translateY(0)' }),
+  animate(100, style({ opacity: 1, transform: 'translateY(-100%)' })),
+];
+
 export const ALL_ANIMATIONS: { [key in IAvailableAnimations]: IAnimation } = {
-  enterAnimationY: ENTER_ANIMATIONS_Y,
-  leaveAnimationY: LEAVE_ANIMATIONS_Y,
+  enterAnimationY: ENTER_ANIMATION_Y,
+  leaveAnimationY: LEAVE_ANIMATION_Y,
+  enterAboveAnimationY: ENTER_ABOVE_ANIMATION_Y,
+  leaveAboveAnimationY: LEAVE_ABOVE_ANIMATION_Y,
 };
--- libs/utils/util-animations/src/lib/models/animation-models.ts
+++ libs/utils/util-animations/src/lib/models/animation-models.ts
@@ -1,4 +1,8 @@
 import { AnimationMetadata } from '@angular/animations';
 
 export type IAnimation = AnimationMetadata | AnimationMetadata[];
-export type IAvailableAnimations = 'enterAnimationY' | 'leaveAnimationY';
+export type IAvailableAnimations =
+  | 'enterAnimationY'
+  | 'leaveAnimationY'
+  | 'enterAboveAnimationY'
+  | 'leaveAboveAnimationY';
