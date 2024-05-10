9dd4c31a85cf0e08db26eca9b4da5acb2650f6a0
pedroping <pedrpchavesdias@gmail.com>
2024-05-09 21:53:04 -0300

    Style: removed metadata config on eslinter

--- libs/utils/util-animations/src/lib/animations/animations.ts
+++ libs/utils/util-animations/src/lib/animations/animations.ts
@@ -3,22 +3,22 @@ import { IAnimation, IAvailableAnimations } from '../models/animation-models';
 
 export const ENTER_ANIMATION_Y: IAnimation = [
   style({ opacity: 0, transform: 'translateY(100%)' }),
-  animate(100, style({ opacity: 1, transform: 'translateY(0)' })),
+  animate(50, style({ opacity: 1, transform: 'translateY(0)' })),
 ];
 
 export const LEAVE_ANIMATION_Y: IAnimation = [
   style({ opacity: 1, transform: 'translateY(0)' }),
-  animate(100, style({ opacity: 1, transform: 'translateY(100%)' })),
+  animate(50, style({ opacity: 1, transform: 'translateY(100%)' })),
 ];
 
 export const ENTER_ABOVE_ANIMATION_Y: IAnimation = [
   style({ opacity: 0, transform: 'translateY(-100%)' }),
-  animate(100, style({ opacity: 1, transform: 'translateY(0)' })),
+  animate(50, style({ opacity: 1, transform: 'translateY(0)' })),
 ];
 
 export const LEAVE_ABOVE_ANIMATION_Y: IAnimation = [
   style({ opacity: 1, transform: 'translateY(0)' }),
-  animate(100, style({ opacity: 1, transform: 'translateY(-100%)' })),
+  animate(50, style({ opacity: 1, transform: 'translateY(-100%)' })),
 ];
 
 export const ALL_ANIMATIONS: { [key in IAvailableAnimations]: IAnimation } = {
