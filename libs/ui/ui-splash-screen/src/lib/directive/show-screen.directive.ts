85310dde1325f65f80a3872bc3c78245c9b6216e
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-06-11 11:02:20 -0300

    Fix: set custom overflow at splash screen

--- libs/ui/ui-splash-screen/src/lib/directive/show-screen.directive.ts
+++ libs/ui/ui-splash-screen/src/lib/directive/show-screen.directive.ts
@@ -22,12 +22,20 @@ export class ShowScreenDirective implements OnInit {
         ('');
       }
     );
+    this.htmlElement.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden';
 
     timer(5000)
       .pipe(take(1))
       .subscribe(() => {
         this.vcr.clear();
         this.buildAnimation.animate('leaveOpacity', location.nativeElement);
+        this.htmlElement.style.overflow = 'auto';
+        document.body.style.overflow = 'auto';
       });
+  }
+
+  get htmlElement() {
+    return document.querySelector('html') as HTMLElement;
   }
 }
