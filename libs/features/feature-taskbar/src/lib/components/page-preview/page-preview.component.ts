1f12ab660128c0851ce34698ff398d401191545b
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-06-14 14:30:02 -0300

    Feat: crate close page on preview

--- libs/features/feature-taskbar/src/lib/components/page-preview/page-preview.component.ts
+++ libs/features/feature-taskbar/src/lib/components/page-preview/page-preview.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { IPreviewPage } from '../../models/preview-models';
-import { IElementReference } from '@portifolio/features/feature-page-creator';
+import {
+  ElementsFacede,
+  IElementReference,
+} from '@portifolio/features/feature-page-creator';
+import { CloseComponent } from '@portifolio/ui/ui-page-actions-buttons';
 
 @Component({
   selector: 'page-preview',
   templateUrl: './page-preview.component.html',
   styleUrls: ['./page-preview.component.scss'],
   standalone: true,
+  imports: [CloseComponent],
 })
 export class PagePreviewComponent implements IPreviewPage {
   element?: IElementReference;
+
+  constructor(private readonly elementsFacade: ElementsFacede) {}
+
+  closeElement() {
+    this.elementsFacade.destroyElement(this.element?.id ?? -1);
+  }
 }
--- src/app/domain/workspace/workspace-page/workspace-page.component.ts
+++ src/app/domain/workspace/workspace-page/workspace-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import {
   InitialMenuCreatorDirective,
   MenuEventsService,
 } from '@portifolio/features/feature-inital-menu';
 import {
   ElementsFacede,
+  IPageMock,
   PageParentDirective,
 } from '@portifolio/features/feature-page-creator';
 @Component({
@@ -14,13 +15,29 @@ import {
   standalone: true,
   imports: [PageParentDirective, InitialMenuCreatorDirective],
 })
-export class WorkspacePageComponent implements OnDestroy {
+export class WorkspacePageComponent implements OnDestroy, AfterViewInit {
   constructor(
     private readonly elementsFacede: ElementsFacede,
     private readonly menuEventsService: MenuEventsService
   ) {}
 
-  ngOnDestroy(): void {
+  ngAfterViewInit() {
+    this.elementsFacede.createElement(
+      {},
+      {
+        name: 'Default Page',
+        customX: 500,
+        customY: 500,
+        baseSizes: { width: 300, height: 200 },
+      },
+      {
+        opened: true,
+        isFullScreen: false,
+      }
+    );
+  }
+
+  ngOnDestroy() {
     this.elementsFacede.clearAll();
     this.menuEventsService.setCloseMenu();
   }
