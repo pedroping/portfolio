d7da003c66aa904172bede0f5a0fe2cdd4b31b43
pedro.henrique <pedro.henrique@brunsker.com.br>
2024-06-27 08:49:29 -0300

    Feat: create app icon lib

---
+++ libs/features/feature-app-icon/src/lib/features-feature-app-icon/features-feature-app-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-features-feature-app-icon',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './features-feature-app-icon.component.html',
+  styleUrl: './features-feature-app-icon.component.css',
+})
+export class FeaturesFeatureAppIconComponent {}
